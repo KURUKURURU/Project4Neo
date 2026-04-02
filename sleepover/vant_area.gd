5b5467c2f7c4bc74b3a7fe488032c3174f0d625f
KURUKURURU <93101194+KURUKURURU@users.noreply.github.com>
2026-04-01 20:36:08 -0400

    added brief crap

--- sleepover/vant_area.gd
+++ sleepover/vant_area.gd
@@ -1,49 +1,83 @@
 extends Node2D
 
 @onready var fadeAnimation = $top/fade/animation
-@onready var fadeColor = $top/fade
 @onready var p = $Player
 @onready var pArea = $Player/Area2D
 @onready var t = $top/talking_ui
 
+@onready var loudfan = $loud
+@onready var bar = $enter_bar
+@onready var knife = $knife
+
 var talking = false
-var enter_bar = false
-var already_talked = false
 
 func _ready() -> void:
 	fadeAnimation.play("fadein")
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("interact") and !talking:
-		if enter_bar:
-			#talking = true
-			p.can_interact = false
-			#t._emote("", "")
-			#await speak("", "", "", "", "")
-			#
-			#e()
-			#talking = false
+func _process(_delta: float) -> void:
+	if talking:
+		return
+	
+	# check loud fan
+	if pArea.overlaps_area(loudfan):
+		p.can_interact = true
+		if Input.is_action_just_pressed("interact"):
+			await loud_fan_dialogue()
+	
+	# check bar
+	elif pArea.overlaps_area(bar):
+		p.can_interact = true
+		if Input.is_action_just_pressed("interact"):
+			await enter_bar_scene()
 			
-			fadeAnimation.play("fade")
-			await fadeAnimation.animation_finished
-			get_tree().change_scene_to_file("res://scenes/kitchen.tscn")
-			
+	elif pArea.overlaps_area(knife):
+		p.can_interact = true
+		if Input.is_action_just_pressed("interact"):
+			await _knife()
+	
+	else:
+		p.can_interact = false
+
+
+# --- interactions ---
+
+func loud_fan_dialogue():
+	talking = true
+	p.can_interact = false
+	
+	t._emote("", "")
+	await speak("You", "This is pretty loud.", "", "", "")
+	await speak("You", "If I compare other's opinions of me, I think I may have competition.", "", "", "")
+	
+	end_dialogue()
+
+func _knife():
+	talking = true
+	p.can_interact = false
+	
+	t._emote("", "")
+	await speak("You", "Pointy. Don't touch.", "", "", "")
+	
+	end_dialogue()
+
+
+
+func enter_bar_scene():
+	p.can_interact = false
+	fadeAnimation.play("fade")
+	await fadeAnimation.animation_finished
+	get_tree().change_scene_to_file("res://scenes/kitchen.tscn")
+
+
+# --- dialogue helpers ---
 
 func speak(n, m, c1, c2, c3):
 	p.moving = false
 	await t._speak(n, m, c1, c2, c3)
 	p.moving = true
-func e():
+
+
+func end_dialogue():
 	t._end()
 	talking = false
 	p.can_interact = true
-	
-func _enter_bar(area: Area2D) -> void:
-	if area == pArea:
-		p.can_interact = true
-		enter_bar = true
-		
-func exit(area):
-	if area == pArea:
-		p.can_interact = false
-		enter_bar = false
